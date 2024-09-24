b7dd26b15f11cd4cd9029981ccc66812e47a41a1
Zomblack3 <santi260406@gmail.com>
2024-09-24 18:33:09 -0300

    Superficial Changes
    Main menu added
    Options added (in progress)
    Difficulties selection added (in progress)

--- Breakout - TP 2/src/main.cpp
+++ Breakout - TP 2/src/main.cpp
@@ -5,6 +5,18 @@
 
 using namespace std;
 
+//Base settings
+const int screenWidth = 460;
+const int screenHeight = 680;
+const int amountObjectives = 81;
+
+enum DIFICULTY
+{
+	EASY,
+	NORMAL,
+	HARD
+};
+
 struct Pos
 {
 	double x = 0.0;
@@ -26,8 +38,8 @@ struct Rectangle
 	Pos pos;
 	int width = 0;
 	int height = 0;
-	int limitMax = 420;
-	int limitMin = 40;
+	int limitMax = screenWidth - 40;
+	int limitMin = screenWidth - 420;
 	bool isObjective = false;
 	bool isActive = true;
 	float r = 0;
@@ -47,28 +59,38 @@ int main()
 {
 	srand(time(0));
 
-	const int screenWidth = 460;
-	const int screenHeight = 680;
-	const int amountObjectives = 41;
+	DIFICULTY dificulty = EASY;
 
+	//Screen specifications
 	int screenCenterX = screenWidth / 2;
 	int screenCenterY = screenHeight / 2;
 
-	int timer = 0;
-	int timerRM = 0;
+	//Timers
+	int startTimer = 0;
+	int collitionTimer = 0;
+	int rmTimer = 0;
+
+	//Gameplay data
 	int score = 0;
 	int lifes = 3;
-	int winConditionScore = 12;
+	int winConditionScore = 81;
 
+	string amountWinCondBlocks = "Puntaje de victoria: " + to_string(winConditionScore) + " [B]";
+
+	//Bools
 	bool rainbowMode = false;
 	bool isDead = false;
+	bool isGameplay = false;
+	bool areOptionsSelected = false;
+	bool isSelectingDificulty = false;
+	bool isSelectingAmountBlocks = false;
 
+	//Objects in game
 	Ball ball;
 	Rectangle player;
 	Rectangle objectives[amountObjectives];
 
-	ball.pos.x = screenCenterX;
-	ball.pos.y = screenCenterY;
+	int baseBallSpeedY = ball.speedY;
 
 	player.width = 80;
 	player.height = 20;
@@ -80,207 +102,327 @@ int main()
 		buildObjective(objectives[i], i, amountObjectives);
 	}
 
-	slWindow(screenWidth, screenHeight, "BREAKOUT", false);
+	slWindow(screenWidth, screenHeight, "BREAKING OUT", false);
 
-	int font = slLoadFont("C:/Users/Aula 1/Desktop/BreakoutRepo/Breakout - TP 2/res/Oswald/static/oswald-medium.ttf");
+	int font = slLoadFont("res/Oswald/static/oswald-medium.ttf");
 
 	while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
 	{
-		string scoreText = "Puntaje: " + to_string(score);
-		string timerRMtext = "Cooldown: " + to_string(timerRM);
-		string lifeText = "Vidas: " + to_string(lifes);
-
-		if (ball.isActive == true)
+		if (!isGameplay)
 		{
-			ball.pos.x += ball.speedX;
-			ball.pos.y += ball.speedY;
+			//Update
+			//-----------------------------------------------------
+
+			//EMPTY
+
+			//Drawing
+			//-----------------------------------------------------
+
+			if (!areOptionsSelected)
+			{
+				slSetForeColor(0, 1, 0, 1);
+
+				slSetFont(font, 50);
+
+				slText(screenCenterX - 145, screenCenterY + 30, "BREAKING OUT");
+
+				slSetForeColor(1, 1, 1, 1);
+
+				slSetFont(font, 20);
+
+				slText(screenCenterX - 110, screenCenterY - 20, "Presione ENTER para iniciar");
+
+				slText(screenCenterX - 140, screenCenterY - 50, "Presione 'o' para entrar en opciones");
+			}
+			else
+			{
+				slSetForeColor(0, 1, 0, 1);
+
+				slSetFont(font, 20);
+
+				slText(screenCenterX - 160, screenCenterY + 70, "OPCIONES");
+
+				slSetForeColor(1, 1, 1, 1);
+
+				if (dificulty == EASY)
+					slText(screenCenterX - 160, screenCenterY + 40, "Dificultad: FACIL [D]");
+				else if (dificulty == NORMAL)
+					slText(screenCenterX - 160, screenCenterY + 40, "Dificultad: NORMAL [D]");
+				else
+					slText(screenCenterX - 160, screenCenterY + 40, "Dificultad: DIFICIL [D]");
+
+				slText(screenCenterX - 160, screenCenterY + 10, amountWinCondBlocks.c_str());
+
+				slText(screenCenterX - 160, screenCenterY - 20, "Presione 'q' para volver");
+			}
+
+			slRender();
+
+			//Input
+			//-----------------------------------------------------
+
+			if (!areOptionsSelected)
+			{
+				if (slGetKey(SL_KEY_ENTER) && !areOptionsSelected)
+				{
+					isGameplay = true;
+					startTimer = 30;
+				}
+
+				if (slGetKey('O'))
+					areOptionsSelected = true;
+			}
+			else
+			{
+				if (slGetKey('Q'))
+					areOptionsSelected = false;
+
+				if (slGetKey('D'))
+					isSelectingDificulty = true;
+
+				if (slGetKey('B'))
+					isSelectingAmountBlocks = true;
+			}
 		}
 		else
 		{
-			ball.pos.x = player.pos.x;
-			ball.pos.y = (player.pos.y * 2) + 15;
-		}
+			//Update
+			//-----------------------------------------------------
 
-		if ((ball.pos.x >= (screenWidth - ball.radius)) || (ball.pos.x <= ball.radius))
-			ball.speedX *= -1.0f;
+			//Texts whit variables
+			string scoreText = "Puntaje: " + to_string(score);
+			string timerRMtext = "Cooldown: " + to_string(rmTimer);
+			string lifeText = "Vidas: " + to_string(lifes);
 
-		if (ball.pos.y >= (screenHeight - ball.radius))
-			ball.speedY *= -1.0f;
-		else if (ball.pos.y <= ball.radius)
-		{
-			ball.pos.x = player.pos.x;
-			ball.pos.y = player.pos.y * 2;
-			ball.isActive = false;
-
-			if (ball.speedY > 0)
-				ball.speedY *= -1.0f;
-
-			if (lifes != 0)
-				--lifes;
-		}
-
-		if (ballHasCollide(ball.pos.x, ball.pos.y, ball.radius, player.pos.x, player.pos.y, player.width, player.height))
-			if (ball.pos.y > player.pos.y && timer == 0)
+			//Ball movement
+			if (ball.isActive == true)
 			{
-				ball.speedY *= -1.0f;
-
-				timer = 5;
+				ball.pos.x += ball.speedX;
+				ball.pos.y += ball.speedY;
 			}
-			else if (ball.pos.y <= player.pos.y && timer == 0)
+			else
 			{
+				ball.pos.x = player.pos.x;
+				ball.pos.y = (player.pos.y * 2) + 15;
+			}
+
+			//Ball limits X collition
+			if ((ball.pos.x >= (screenWidth - ball.radius)) || (ball.pos.x <= ball.radius))
 				ball.speedX *= -1.0f;
 
-				timer = 5;
+			//Ball limits Y collition
+			if (ball.pos.y >= (screenHeight - ball.radius)) //Up limit
+				ball.speedY *= -1.0f;
+			else if (ball.pos.y <= ball.radius) //Down limit
+			{
+				ball.pos.x = player.pos.x;
+				ball.pos.y = player.pos.y * 2;
+				ball.isActive = false;
+
+				ball.speedY = baseBallSpeedY;
+
+				if (ball.speedY > 0)
+					ball.speedY *= -1.0f;
+
+				if (lifes != 0)
+					--lifes;
 			}
 
-		for (int i = 0; i < amountObjectives; i++)
-			if (ballHasCollide(ball.pos.x, ball.pos.y, ball.radius, objectives[i].pos.x, objectives[i].pos.y, objectives[i].width, objectives[i].height))
-				if (objectives[i].isActive)
+			//Ball & Player collition
+			if (ballHasCollide(ball.pos.x, ball.pos.y, ball.radius, player.pos.x, player.pos.y, player.width, player.height))
+				if (ball.pos.y > player.pos.y && collitionTimer == 0)
 				{
-					objectives[i].isActive = false;
 					ball.speedY *= -1.0f;
-					++score;
+
+					collitionTimer = 5;
+				}
+				else if (ball.pos.y <= player.pos.y && collitionTimer == 0)
+				{
+					ball.speedX *= -1.0f;
+
+					collitionTimer = 5;
 				}
 
-		if (areAllBlocksDead(objectives, amountObjectives))
-		{
+			//Ball & Objectives collition
 			for (int i = 0; i < amountObjectives; i++)
+				if (ballHasCollide(ball.pos.x, ball.pos.y, ball.radius, objectives[i].pos.x, objectives[i].pos.y, objectives[i].width, objectives[i].height))
+					if (objectives[i].isActive)
+					{
+						objectives[i].isActive = false;
+
+						if (ball.speedY < 0)
+						{
+							ball.speedY -= 0.05f;
+							//ball.speedX -= 0.01f;
+						}
+						else
+						{
+							ball.speedY += 0.05f;
+							//ball.speedX += 0.01f;
+						}
+
+						ball.speedY *= -1.0f;
+						++score;
+					}
+
+			//Objectives respawn
+			if (areAllBlocksDead(objectives, amountObjectives))
 			{
-				objectives[i].isActive = true;
-				objectives[i].r = randColor();
-				objectives[i].g = randColor();
-				objectives[i].b = randColor();
+				ball.pos.x = player.pos.x;
+				ball.pos.y = player.pos.y * 2;
+				ball.isActive = false;
+
+				if (ball.speedY > 0)
+					ball.speedY *= -1.0f;
+
+				for (int i = 0; i < amountObjectives; i++)
+				{
+					objectives[i].isActive = true;
+					objectives[i].r = randColor();
+					objectives[i].g = randColor();
+					objectives[i].b = randColor();
+				}
 			}
-		}
 
-		if (timer != 0)
-			--timer;
+			//Timers update
+			//------------------------
+			if (startTimer != 0)
+				--startTimer;
 
-		if (timerRM != 0)
-			--timerRM;
+			if (collitionTimer != 0)
+				--collitionTimer;
 
-		if (lifes == 0)
-			isDead = true;
+			if (rmTimer != 0)
+				--rmTimer;
 
-		if (ball.speedY < 0 && ball.isActive == false)
-			ball.speedY *= -1.0f;
+			if (lifes == 0)
+				isDead = true;
+			//------------------------
 
-		if (isDead || score >= winConditionScore)
-		{
-			ball.isActive = false;
+			//Security check for ball starting (if the ball is going down when starts, change ball direction)
+			if (ball.speedY < 0 && ball.isActive == false)
+				ball.speedY *= -1.0f;
 
-			ball.pos.x = player.pos.x;
-			ball.pos.y = (player.pos.y * 2) + 15;
+			//Win & Defeat condition update if it's true
+			if (isDead || score >= winConditionScore)
+			{
+				ball.isActive = false;
 
-			for (int i = 0; i < amountObjectives; i++)
-				objectives[i].isActive = true;
-		}
+				ball.pos.x = player.pos.x;
+				ball.pos.y = (player.pos.y * 2) + 15;
 
-		//----------------------------------------
+				for (int i = 0; i < amountObjectives; i++)
+					objectives[i].isActive = true;
+			}
 
-		if (!isDead && score != winConditionScore)
-		{
-			if (!rainbowMode)
-				slSetForeColor(1, 1, 1, 1.5f);
-			else
-				slSetForeColor(randColor(), randColor(), randColor(), 1.5f);
+			//Drawing
+			//----------------------------------------
 
-			slSetFont(font, 20);
-
-			slText(objectives->limitMin, 640, scoreText.c_str());
-
-			slText(objectives->limitMin, 610, lifeText.c_str());
-
-			if (timerRM != 0)
-				slText(330, 640, timerRMtext.c_str());
-
-			if (!rainbowMode)
-				slSetForeColor(0.1f, 0.5f, 0.9f, 0.4f);
-			else
-				slSetForeColor(randColor(), randColor(), randColor(), 1.5f);
-
-			slRectangleFill(player.pos.x, player.pos.y, player.width, player.height);
-
-			slCircleFill(ball.pos.x, ball.pos.y, ball.radius, ball.numVertices);
-
-			for (int i = 0; i < amountObjectives; i++)
+			if (!isDead && score != winConditionScore)
 			{
 				if (!rainbowMode)
-					slSetForeColor(objectives[i].r, objectives[i].g, objectives[i].b, 1.5f);
+					slSetForeColor(1, 1, 1, 1.5f);
 				else
 					slSetForeColor(randColor(), randColor(), randColor(), 1.5f);
 
-				if (objectives[i].isActive)
-					slRectangleFill(objectives[i].pos.x, objectives[i].pos.y, objectives[i].width, objectives[i].height);
-			}
-		}
-		else if (isDead)
-		{
-			slSetForeColor(1, 0, 0, 1.5f);
+				slSetFont(font, 20);
 
-			slSetFont(font, 30);
+				slText(objectives->limitMin, 640, scoreText.c_str());
 
-			slText(screenCenterX - 90, screenCenterY, "HAS MUERTO!!!!");
+				slText(objectives->limitMin, 610, lifeText.c_str());
 
-			slSetForeColor(1, 1, 1, 1.5f);
+				if (rmTimer != 0)
+					slText(330, 640, timerRMtext.c_str());
 
-			slSetFont(font, 15);
-
-			slText(screenCenterX - 75, screenCenterY - 30, "Presione R para reiniciar");
-		}
-		else if (score >= winConditionScore)
-		{
-			slSetForeColor(0, 1, 0, 1.5f);
-
-			slSetFont(font, 30);
-
-			slText(screenCenterX - 70, screenCenterY, "GANASTE!!!!");
-
-			slSetForeColor(1, 1, 1, 1.5f);
-
-			slSetFont(font, 15);
-
-			slText(screenCenterX - 75, screenCenterY - 30, "Presione R para reiniciar");
-		}
-
-		slRender();
-
-		//----------------------------------------
-
-		if (!isDead && score != winConditionScore)
-		{
-			if (slGetKey(SL_KEY_RIGHT))
-				if (player.pos.x != player.limitMax)
-					player.pos.x += 5;
-
-			if (slGetKey(SL_KEY_LEFT))
-				if (player.pos.x != player.limitMin)
-					player.pos.x -= 5;
-
-			if (slGetKey(SL_KEY_ENTER))
-				ball.isActive = true;
-
-			if (slGetKey('T') && timerRM == 0)
-				if (rainbowMode)
-				{
-					rainbowMode = false;
-
-					timerRM = 100;
-				}
+				if (!rainbowMode)
+					slSetForeColor(0.1f, 0.5f, 0.9f, 0.4f);
 				else
-				{
-					rainbowMode = true;
+					slSetForeColor(randColor(), randColor(), randColor(), 1.5f);
 
-					timerRM = 100;
+				slRectangleFill(player.pos.x, player.pos.y, player.width, player.height);
+
+				slCircleFill(ball.pos.x, ball.pos.y, ball.radius, ball.numVertices);
+
+				for (int i = 0; i < amountObjectives; i++)
+				{
+					if (!rainbowMode)
+						slSetForeColor(objectives[i].r, objectives[i].g, objectives[i].b, 1.5f);
+					else
+						slSetForeColor(randColor(), randColor(), randColor(), 1.5f);
+
+					if (objectives[i].isActive)
+						slRectangleFill(objectives[i].pos.x, objectives[i].pos.y, objectives[i].width, objectives[i].height);
 				}
-		}
-		else
-		{
-			if (slGetKey('R'))
+			}
+			else if (isDead)
 			{
-				isDead = false;
-				score = 0;
-				lifes = 3;
+				slSetForeColor(1, 0, 0, 1.5f);
+
+				slSetFont(font, 30);
+
+				slText(screenCenterX - 90, screenCenterY, "HAS MUERTO!!!!");
+
+				slSetForeColor(1, 1, 1, 1.5f);
+
+				slSetFont(font, 15);
+
+				slText(screenCenterX - 75, screenCenterY - 30, "Presione R para reiniciar");
+			}
+			else if (score >= winConditionScore)
+			{
+				slSetForeColor(0, 1, 0, 1.5f);
+
+				slSetFont(font, 30);
+
+				slText(screenCenterX - 70, screenCenterY, "GANASTE!!!!");
+
+				slSetForeColor(1, 1, 1, 1.5f);
+
+				slSetFont(font, 15);
+
+				slText(screenCenterX - 75, screenCenterY - 30, "Presione R para reiniciar");
+			}
+
+			slRender();
+
+			//Input
+			//----------------------------------------
+
+			if (!isDead && score != winConditionScore)
+			{
+				if (slGetKey(SL_KEY_RIGHT))
+					if (player.pos.x != player.limitMax)
+						player.pos.x += 5;
+
+				if (slGetKey(SL_KEY_LEFT))
+					if (player.pos.x != player.limitMin)
+						player.pos.x -= 5;
+
+				if (slGetKey(SL_KEY_ENTER) && startTimer == 0)
+					ball.isActive = true;
+
+				if (slGetKey('T') && rmTimer == 0)
+					if (rainbowMode)
+					{
+						rainbowMode = false;
+
+						rmTimer = 100;
+					}
+					else
+					{
+						rainbowMode = true;
+
+						rmTimer = 100;
+					}
+			}
+			else
+			{
+				if (slGetKey('R'))
+				{
+					isDead = false;
+					score = 0;
+					lifes = 3;
+				}
 			}
 		}
 	}
@@ -302,7 +444,7 @@ void buildObjective(Rectangle& obj, int actualObj, int amountObj)
 	static bool reachLimit = false;
 	static bool hadStop = false;
 	static int posX = 10;
-	static int posY = 500;
+	static int posY = 400;
 
 	int modNumX = 40;
 	int modNumY = 25;
@@ -317,6 +459,8 @@ void buildObjective(Rectangle& obj, int actualObj, int amountObj)
 	obj.pos.x = obj.limitMax - posX;
 	obj.pos.y = posY;
 
+	//Objectives position algorithm
+	//---------------------------------------------
 	if (actualObj % 10 == 0)
 	{
 		if (!hadStop)
@@ -332,6 +476,7 @@ void buildObjective(Rectangle& obj, int actualObj, int amountObj)
 			posX += modNumX;
 		else
 			posX -= modNumX;
+	//---------------------------------------------
 }
 
 bool areAllBlocksDead(Rectangle obj[], int amountObjs)
